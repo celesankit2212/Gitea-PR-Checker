b8472937e63d6e44811fc34ea47def002cce2998
celesankit2212 <67006536+celesankit2212@users.noreply.github.com>
2023-03-17 12:38:48 +0530

    Update pr_checker.py

--- pr_checker.py
+++ pr_checker.py
@@ -8,22 +8,36 @@ HEADERS = ({'User-Agent':
             (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
 
 # put range of PRs
-for i in range(202, 238):
+for i in range(202, 242):
     res = requests.get('https://gitea-working.testrail-staging.com/Gurock/automation-testrail/pulls/'+str(i),
-                       auth=HTTPBasicAuth('AnkitM', 'Ai987$%tM'), headers=HEADERS)
-    print("The status code is ", res.status_code)
+                       auth=HTTPBasicAuth('<your username>', '<your password>'), headers=HEADERS)
+    if res.status_code != 200:
+        print("The status code is ", res.status_code)
     soup_data = BeautifulSoup(res.text, 'html.parser')
-    print(soup_data.title)
+    title_text = soup_data.title
+    print("Working on Title : " + str(title_text).replace('<title>', '').replace('</title>', '') + "\n")
     tree = html.fromstring(res.content)
     try:
-        assignee = tree.xpath("//div[@class='ui assignees list']//span[contains(@class,'yellow')]/../a/text()")[-1]
+        pr_raised_by = tree.xpath("//span[@id='pull-desc']/a/text()")[0]
+        print(f"Current PR#{i} raised by : " + str(pr_raised_by) + "\n")
         try:
-            branch_update = tree.xpath("//button[@data-do='update']")
-            print(f"PR# {i} needs to be updated")
+            branch_update = tree.xpath("//button[@data-do='update']/span/text()")[0]
+            print(f"PR# {i} needs to be updated as received '"+str(branch_update)+"' from PR")
         except Exception as error:
-            print(f"As Error {error} Received for PR# : " + str(i) + "means no update branch required")
-        print("PR in review with : "+str(assignee).replace('\t', '').replace('\n', ''))
-    except Exception as error:
-        print(f"Error {error} Received for PR# regarding assignee: " + str(i))
+            print(f"As Error {error} Received no PR update for PR# :".replace(
+                'As Error list index out of range ', '') + str(i) + "\nmeans no update branch required\n")
+            try:
+                assignee = tree.xpath(
+                    "//div[@class='review-item' and .//span[contains(@class,'yellow')]]/div/span/a/text()")[0]
+                time_opened = tree.xpath(
+                    "//div[@class='review-item' and .//span[contains(@class,'yellow')]]/div/span/span/text()")[0]
+
+                print("PR in review with : " + str(assignee) + "\nfrom : " + str(time_opened))
+            except Exception as error:
+                print(f"Error {error} Received other request for PR# : ".replace(
+                    'Error list index out of range ', '') + str(i) + "\nmeans PR not in review")
+    except IndexError:
+        print("PR seems to be closed or merged")
+
     print("\n\n-------------------------------------------------------")
     continue
