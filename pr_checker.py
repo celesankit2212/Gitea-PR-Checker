8155d114dd8bcf3af462743952457ec001ab6fe8
celesankit2212 <67006536+celesankit2212@users.noreply.github.com>
2023-03-17 10:24:00 +0530

    Update pr_checker.py

--- pr_checker.py
+++ pr_checker.py
@@ -8,17 +8,22 @@ HEADERS = ({'User-Agent':
             (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
 
 # put range of PRs
-for i in range(<start>, <end plus one>):
+for i in range(202, 238):
     res = requests.get('https://gitea-working.testrail-staging.com/Gurock/automation-testrail/pulls/'+str(i),
-                       auth=HTTPBasicAuth('<your username for Gitea>', '<your password for Gitea>'), headers=HEADERS)
+                       auth=HTTPBasicAuth('AnkitM', 'Ai987$%tM'), headers=HEADERS)
     print("The status code is ", res.status_code)
     soup_data = BeautifulSoup(res.text, 'html.parser')
     print(soup_data.title)
     tree = html.fromstring(res.content)
     try:
         assignee = tree.xpath("//div[@class='ui assignees list']//span[contains(@class,'yellow')]/../a/text()")[-1]
+        try:
+            branch_update = tree.xpath("//button[@data-do='update']")
+            print(f"PR# {i} needs to be updated")
+        except Exception as error:
+            print(f"As Error {error} Received for PR# : " + str(i) + "means no update branch required")
         print("PR in review with : "+str(assignee).replace('\t', '').replace('\n', ''))
     except Exception as error:
-        print(f"Error {error} Received for PR# : " + str(i))
+        print(f"Error {error} Received for PR# regarding assignee: " + str(i))
     print("\n\n-------------------------------------------------------")
     continue
