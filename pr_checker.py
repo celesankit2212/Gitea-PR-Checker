b516852c6ee8c0a3c5d86cbeea83dfbc30a9429e
celesankit2212 <67006536+celesankit2212@users.noreply.github.com>
2023-03-23 13:12:44 +0530

    Update pr_checker.py

--- pr_checker.py
+++ pr_checker.py
@@ -20,60 +20,84 @@ raised_review_from = []
 having_conflicts = []
 username = textinput("Username", "Please provide your Gitea username !!")
 password = textinput("Password", "Please provide your Gitea password !!")
+last_pr = textinput("Last PR Number Raised", "Please provide last PR number raised")
 tr = turtle.Turtle()
 wn = turtle.Screen()
 wn.addshape('report_processing.gif')
 tr.shape('report_processing.gif')
 # put range of PRs
-for pr_number in range(202, 245):
+for pr_number in range(202, int(last_pr)):
     res = requests.get('https://gitea-working.testrail-staging.com/Gurock/automation-testrail/pulls/'+str(pr_number),
                        auth=HTTPBasicAuth(username, password), headers=HEADERS)
     if res.status_code != 200:
         print("The status code is ", res.status_code)
     url = res.url
-    pr_urls.append(url)
     soup_data = BeautifulSoup(res.text, 'html.parser')
     title_text = soup_data.title
-    print("Working on Title : PR" + str(title_text).replace('<title>', '').replace('</title>', '').replace(
-        '-  automation-testrail - Gitea: Git with a cup of tea', '') + "\n")
-    title.append("PR" + str(title_text).replace('<title>', '').replace('</title>', '').replace(
-        '-  automation-testrail - Gitea: Git with a cup of tea', ''))
     tree = html.fromstring(res.content)
+    celestial_pr = tree.xpath("//code[2]/a/text()")[0]
     pr_status = tree.xpath("//div[contains(@class,'title')]/div[contains(@class, 'large label')]/text()")[0]
-    current_pr_status.append(pr_status)
-    print("Status of PR#" + str(pr_number) + " is : " + str(pr_status))
-    try:
-        pr_raised_by = tree.xpath("//span[@id='pull-desc']/a/text()")[0]
-        pr_raiser.append(pr_raised_by)
-        print(f"Current PR#{pr_number} raised by : " + str(pr_raised_by) + "\n")
+    if 'Celestial_New_Automation_Enhancement' in celestial_pr and 'Closed' not in pr_status:
+        print("Working on Title : PR" + str(title_text).replace('<title>', '').replace('</title>', '').replace(
+            '-  automation-testrail - Gitea: Git with a cup of tea', '') + "\n")
+        title.append("PR" + str(title_text).replace('<title>', '').replace('</title>', '').replace(
+            '-  automation-testrail - Gitea: Git with a cup of tea', ''))
+        pr_urls.append(url)
+        current_pr_status.append(pr_status)
+        print("Status of PR#" + str(pr_number) + " is : " + str(pr_status))
         try:
-            branch_update = tree.xpath("//button[@data-do='update']/span/text()")[0]
-            branch_update_request.append("True")
-            having_conflicts.append("NA")
-            reviewer.append("NA")
-            raised_review_from.append("NA")
-            print(f"PR# {pr_number} needs to be updated as received '"+str(
-                branch_update).replace('\t', '').replace('\n', '')+"' from PR")
-        except Exception as error:
-            branch_update_request.append("False")
-            print(f"As Error {error} Received no PR update for PR# :".replace(
-                'As Error list index out of range ', '') + str(pr_number) + "\nmeans no update branch required\n")
+            pr_raised_by = tree.xpath("//span[@id='pull-desc']/a/text()")[0]
+            pr_raiser.append(pr_raised_by)
+            print(f"Current PR#{pr_number} raised by : " + str(pr_raised_by) + "\n")
             try:
-                conflict = tree.xpath("//div[contains(@class,'merge-section')]/div/text()")[1]
-                if 'conflicting' in conflict:
-                    print(f"PR# {pr_number} needs to checked with conflicts as received '" + str(conflict) + "' from PR")
-                    having_conflicts.append("True")
-                    reviewer.append("NA")
-                    raised_review_from.append("NA")
-                else:
+                branch_update = tree.xpath("//button[@data-do='update']/span/text()")[0]
+                branch_update_request.append("True")
+                having_conflicts.append("NA")
+                reviewer.append("NA")
+                raised_review_from.append("NA")
+                print(f"PR# {pr_number} needs to be updated as received '"+str(
+                    branch_update).replace('\t', '').replace('\n', '')+"' from PR")
+            except Exception as error:
+                branch_update_request.append("False")
+                print(f"As Error {error} Received no PR update for PR# :".replace(
+                    'As Error list index out of range ', '') + str(pr_number) + "\nmeans no update branch required\n")
+                try:
+                    conflict = tree.xpath("//div[contains(@class,'merge-section')]/div/text()")[1].replace(
+                        '\t', '').replace('\n', '')
+                    if 'conflicting' in conflict:
+                        print(f"PR# {pr_number} needs to checked with conflicts as received '" + str(
+                            conflict) + "' from PR")
+                        having_conflicts.append("True")
+                        reviewer.append("NA")
+                        raised_review_from.append("NA")
+                    else:
+                        having_conflicts.append("False")
+                        print(f"PR# {pr_number} having No conflicts as received '" + str(conflict) + "' from PR")
+                        try:
+                            assignee = tree.xpath(
+                                "//div[@class='review-item' and .//span[contains(@class,'yellow')]]/div/span/a/text()")[0]
+                            time_opened = tree.xpath(
+                                "//div[@class='review-item' and .//span[contains(@class,'yellow')]]/div/span/span/text()")[
+                                0]
+                            reviewer.append(assignee)
+                            raised_review_from.append(time_opened)
+                            print("PR in review with : " + str(assignee) + "\nfrom : " + str(time_opened))
+                        except Exception as error:
+                            reviewer.append("NA")
+                            raised_review_from.append("NA")
+                            print(f"Error {error} Received other request for PR# : ".replace(
+                                'Error list index out of range ', '') + str(pr_number) + "\nmeans PR not in review")
+                except IndexError:
                     having_conflicts.append("False")
-                    print(f"PR# {pr_number} having No conflicts as received '" + str(conflict) + "' from PR")
                     try:
                         assignee = tree.xpath(
                             "//div[@class='review-item' and .//span[contains(@class,'yellow')]]/div/span/a/text()")[0]
                         time_opened = tree.xpath(
-                            "//div[@class='review-item' and .//span[contains(@class,'yellow')]]/div/span/span/text()")[
-                            0]
+                            "//div[@class='review-item' and .//span[contains(@class,'yellow')]]/div/span/span/text()")[0]
+                        if 'julia' in assignee:
+                            assignee = assignee + " (External Review)"
+                        else:
+                            assignee = assignee + " (Internal Review)"
                         reviewer.append(assignee)
                         raised_review_from.append(time_opened)
                         print("PR in review with : " + str(assignee) + "\nfrom : " + str(time_opened))
@@ -82,43 +106,25 @@ for pr_number in range(202, 245):
                         raised_review_from.append("NA")
                         print(f"Error {error} Received other request for PR# : ".replace(
                             'Error list index out of range ', '') + str(pr_number) + "\nmeans PR not in review")
-            except IndexError:
-                having_conflicts.append("False")
-                try:
-                    assignee = tree.xpath(
-                        "//div[@class='review-item' and .//span[contains(@class,'yellow')]]/div/span/a/text()")[0]
-                    time_opened = tree.xpath(
-                        "//div[@class='review-item' and .//span[contains(@class,'yellow')]]/div/span/span/text()")[0]
-                    if 'julia' in assignee:
-                        assignee = assignee + " (External Review)"
-                    else:
-                        assignee = assignee + " (Internal Review)"
-                    reviewer.append(assignee)
-                    raised_review_from.append(time_opened)
-                    print("PR in review with : " + str(assignee) + "\nfrom : " + str(time_opened))
-                except Exception as error:
-                    reviewer.append("NA")
-                    raised_review_from.append("NA")
-                    print(f"Error {error} Received other request for PR# : ".replace(
-                        'Error list index out of range ', '') + str(pr_number) + "\nmeans PR not in review")
-    except IndexError:
-        try:
-            pr_by = tree.xpath("//div[contains(@class, 'comment first')]//div[contains(@class, 'comment-header')]/div[./div[contains(.,'Collaborator') or contains(.,'Owner')]]/preceding-sibling::div//a[@class='author']/text()")[0]
-            effort_type = tree.xpath("//div[contains(@class, 'comment first')]//div[contains(@class, 'comment-header')]/div[./div[contains(.,'Collaborator') or contains(.,'Owner')]]/div[1]/text()")[0].replace('\t', '').replace('\n', '')
-            if 'Merged' in pr_status:
-                pr_raiser.append(str(effort_type) + " - " + str(pr_by))
-            else:
-                pr_raiser.append("NA")
         except IndexError:
-            pr_raiser.append("NA")
-        branch_update_request.append("NA")
-        having_conflicts.append("NA")
-        reviewer.append("NA")
-        raised_review_from.append("NA")
-        print("PR seems to be closed or merged")
+            try:
+                pr_by = tree.xpath("//div[contains(@class, 'comment first')]//div[contains(@class, 'comment-header')]/div[./div[contains(.,'Collaborator') or contains(.,'Owner')]]/preceding-sibling::div//a[@class='author']/text()")[0]
+                effort_type = tree.xpath("//div[contains(@class, 'comment first')]//div[contains(@class, 'comment-header')]/div[./div[contains(.,'Collaborator') or contains(.,'Owner')]]/div[1]/text()")[0].replace('\t', '').replace('\n', '')
+                if 'Merged' in pr_status:
+                    pr_raiser.append(str(effort_type) + " - " + str(pr_by))
+                else:
+                    pr_raiser.append("NA")
+            except IndexError:
+                pr_raiser.append("NA")
+            branch_update_request.append("NA")
+            having_conflicts.append("NA")
+            reviewer.append("NA")
+            raised_review_from.append("NA")
+            print("PR seems to be closed or merged")
 
-    print("\n\n-------------------------------------------------------")
-    continue
+        print("\n\n-------------------------------------------------------")
+    else:
+        continue
 
 # dictionary of lists
 dictionary_frame = {'Title': title, 'PR_Links': pr_urls, 'Current PR Status': current_pr_status, 'Raised By': pr_raiser,
