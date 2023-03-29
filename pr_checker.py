d1986bcd2277717f509593ab6858508536e23517
celesankit2212 <67006536+celesankit2212@users.noreply.github.com>
2023-03-29 18:25:46 +0530

    Update pr_checker.py

--- pr_checker.py
+++ pr_checker.py
@@ -5,7 +5,17 @@ from requests.auth import HTTPBasicAuth
 from lxml import html
 import pandas as pd
 import openpyxl
+import random
 from turtle import textinput
+
+
+headers = requests.utils.default_headers()
+
+headers.update(
+    {
+        'User-Agent': f'My User Agent {random.randint(5, 20)}.0',
+    }
+)
 
 HEADERS = ({'User-Agent':
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
@@ -18,17 +28,23 @@ branch_update_request = []
 reviewer = []
 raised_review_from = []
 having_conflicts = []
+collaborators = ['AnkitM', 'Preetipt', 'SagarSatishSonwalkar', 'RameshR', 'ArchanaMurthy']
+pr_merge_ankit = pr_merge_preeti = pr_merge_ramesh = pr_merge_archana = pr_merge_sagar = 0
+pr_open_ankit = pr_open_preeti = pr_open_ramesh = pr_open_archana = pr_open_sagar = 0
 username = textinput("Username", "Please provide your Gitea username !!")
 password = textinput("Password", "Please provide your Gitea password !!")
 last_pr = textinput("Last PR Number Raised", "Please provide last PR number raised")
+required_values = {'username': str(username), 'password': str(password), 'last_pr': last_pr}
 tr = turtle.Turtle()
 wn = turtle.Screen()
 wn.addshape('report_processing.gif')
 tr.shape('report_processing.gif')
 # put range of PRs
-for pr_number in range(202, int(last_pr)):
+for pr_number in range(202, int(required_values.get('last_pr'))):
     res = requests.get('https://gitea-working.testrail-staging.com/Gurock/automation-testrail/pulls/'+str(pr_number),
-                       auth=HTTPBasicAuth(username, password), headers=HEADERS)
+                       auth=HTTPBasicAuth(
+                           required_values.get('username'),
+                           required_values.get('password')), headers=headers)
     if res.status_code != 200:
         print("The status code is ", res.status_code)
     url = res.url
@@ -49,6 +65,16 @@ for pr_number in range(202, int(last_pr)):
             pr_raised_by = tree.xpath("//span[@id='pull-desc']/a/text()")[0]
             pr_raiser.append(pr_raised_by)
             print(f"Current PR#{pr_number} raised by : " + str(pr_raised_by) + "\n")
+            if 'AnkitM' in pr_raised_by:
+                pr_open_ankit = pr_open_ankit + 1
+            elif 'Preetipt' in pr_raised_by:
+                pr_open_preeti = pr_open_preeti + 1
+            elif 'SagarSatishSonwalkar' in pr_raised_by:
+                pr_open_sagar = pr_open_sagar + 1
+            elif 'RameshR' in pr_raised_by:
+                pr_open_ramesh = pr_open_ramesh + 1
+            elif 'ArchanaMurthy' in pr_raised_by:
+                pr_open_archana = pr_open_archana + 1
             try:
                 branch_update = tree.xpath("//button[@data-do='update']/span/text()")[0]
                 branch_update_request.append("True")
@@ -112,6 +138,16 @@ for pr_number in range(202, int(last_pr)):
                 effort_type = tree.xpath("//div[contains(@class, 'comment first')]//div[contains(@class, 'comment-header')]/div[./div[contains(.,'Collaborator') or contains(.,'Owner')]]/div[1]/text()")[0].replace('\t', '').replace('\n', '')
                 if 'Merged' in pr_status:
                     pr_raiser.append(str(effort_type) + " - " + str(pr_by))
+                    if 'AnkitM' in pr_by:
+                        pr_merge_ankit = pr_merge_ankit + 1
+                    elif 'Preetipt' in pr_by:
+                        pr_merge_preeti = pr_merge_preeti + 1
+                    elif 'SagarSatishSonwalkar' in pr_by:
+                        pr_merge_sagar = pr_merge_sagar + 1
+                    elif 'RameshR' in pr_by:
+                        pr_merge_ramesh = pr_merge_ramesh + 1
+                    elif 'ArchanaMurthy' in pr_by:
+                        pr_merge_archana = pr_merge_archana + 1
                 else:
                     pr_raiser.append("NA")
             except IndexError:
@@ -131,8 +167,23 @@ dictionary_frame = {'Title': title, 'PR_Links': pr_urls, 'Current PR Status': cu
                     'Branch Update Required': branch_update_request, 'Having Conflicts': having_conflicts,
                     'Reviewer': reviewer, 'Raised to Reviewer from': raised_review_from}
 
-dataframe = pd.DataFrame(dictionary_frame)
+collaborators_frame = {'Collaborator': collaborators,
+                       'Open PRs': [pr_open_ankit, pr_open_preeti, pr_open_sagar, pr_open_ramesh, pr_open_archana],
+                       'Merged PRs': [pr_merge_ankit, pr_merge_preeti, pr_merge_sagar, pr_merge_ramesh, pr_merge_archana],
+                       'Total PRs': [pr_open_ankit + pr_merge_ankit,
+                                     pr_open_preeti + pr_merge_preeti,
+                                     pr_open_sagar + pr_merge_sagar,
+                                     pr_open_ramesh + pr_merge_ramesh,
+                                     pr_open_archana + pr_merge_archana]}
 
+dataframe = pd.DataFrame(dictionary_frame)
+dataframe_collab = pd.DataFrame(collaborators_frame)
 # saving the dataframe
 dataframe.to_csv('Report_New.csv', index=False)
-dataframe.to_excel('Report_New.xlsx', sheet_name='Current PR Status', index=False)
+dataframe_collab.to_csv('Report_Collab.csv', index=False)
+dataframe.to_excel('Report_Check.xlsx', sheet_name='Current PR Status', index=False)
+dataframe_collab.to_excel('Report_Collab.xlsx', sheet_name='Collaborators Status', index=False)
+
+with pd.ExcelWriter('Report_Final.xlsx') as writer:
+    dataframe.to_excel(writer, sheet_name='Current PR Status', index=False)
+    dataframe_collab.to_excel(writer, sheet_name='Collaborators Status', index=False)
